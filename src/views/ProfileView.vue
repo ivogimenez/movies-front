94d836e9c6f077351f1a88951c85c8fce403241b
Nicokevo <82186948+Nicokevo@users.noreply.github.com>
2023-11-20 22:15:02 -0300

    push profile

--- src/views/ProfileView.vue
+++ src/views/ProfileView.vue
@@ -25,43 +25,42 @@ export default {
     };
   },
   created() {
-    // Verificar la autenticación al crear el componente
+  
     this.checkAuthentication();
   },
   methods: {
     async checkAuthentication() {
-      // Lógica para verificar la autenticación (puede variar según tu implementación)
-      // Por ejemplo, podrías hacer una solicitud a tu API para obtener información del usuario
+    
       try {
-        const response = await axios.get('URL_DE_TU_API/usuario'); // Reemplaza con la URL real de tu API
+        const response = await axios.get('URL_DE_TU_API/usuario'); 
         this.user = response.data;
         this.isLoggedIn = true;
       } catch (error) {
-        // Si hay un error al obtener la información del usuario, el usuario no está autenticado
+       
         console.error('Usuario no autenticado:', error);
         this.isLoggedIn = false;
       }
     },
     async changePassword() {
-      // Lógica para cambiar la contraseña
+  
       console.log(`Contraseña cambiada a: ${this.newPassword}`);
       this.newPassword = '';
     },
     async updateDescription() {
-      // Lógica para actualizar la descripción
+  
       console.log(`Nueva Descripción: ${this.newDescription} para el perfil del usuario`);
       this.newDescription = '';
-      // Aquí puedes realizar una solicitud para actualizar la descripción en la base de datos
+    
     },
     async deleteDescription() {
-      // Lógica para eliminar la descripción
+    
       console.log('Descripción eliminada para el perfil del usuario');
-      // Aquí puedes realizar una solicitud para eliminar la descripción en la base de datos
+   
     },
   },
 };
 </script>
 
 <style scoped>
-/* Agrega estilos adicionales si es necesario */
+
 </style>
